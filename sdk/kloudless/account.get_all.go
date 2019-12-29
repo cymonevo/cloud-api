1428d98cdd14bd14d79861a00ba3aaa10693d1dc
cymon1997 <cybermonster82@yahoo.com>
2019-12-29 19:06:14 +0700

    implement kloudless folder api

--- sdk/kloudless/account.get_all.go
+++ sdk/kloudless/account.get_all.go
@@ -3,7 +3,6 @@ package kloudless
 import (
 	"context"
 	"errors"
-	"fmt"
 	"net/http"
 
 	"github.com/cymonevo/cloud-api/handler"
@@ -13,21 +12,19 @@ import (
 const getAllAccounts = "/accounts"
 
 func (c *clientImpl) GetAccounts(ctx context.Context) (interface{}, error) {
-	resp, err := c.client.Get(getAllAccounts, nil, map[string]string{
-		"Authorization": fmt.Sprint("APIKey", c.APIKey),
-	})
+	resp, err := c.client.Get(getAllAccounts, nil, c.headers(nil))
 	if err != nil {
-		log.ErrorDetail("GetAccount", "error get accounts", err)
+		log.ErrorDetail("GetAccounts", "error get accounts", err)
 		return nil, err
 	}
 	if resp.StatusCode != http.StatusOK {
-		log.Warnf("SDK", "status %d %s", resp.StatusCode, resp.Status)
+		log.Warnf("GetAccounts", "status %d %s", resp.StatusCode, resp.Status)
 		return nil, errors.New(resp.Status)
 	}
 	var data interface{}
 	err = handler.ParseBody(resp.Body, &data)
 	if err != nil {
-		log.ErrorDetail("SDK", "error unmarshal data", err)
+		log.ErrorDetail("GetAccounts", "error unmarshal data", err)
 		return nil, err
 	}
 	log.Infof("SDK", "success get accounts %+v", data)
--- sdk/kloudless/client.go
+++ sdk/kloudless/client.go
@@ -2,21 +2,32 @@ package kloudless
 
 import (
 	"context"
+	"fmt"
+
+	"github.com/cymonevo/cloud-api/internal/util"
 	"github.com/cymonevo/cloud-api/sdk"
 )
 
 type Client interface {
 	GetAccounts(ctx context.Context) (interface{}, error)
-	//CreateFolder()
+	CreateFolder(ctx context.Context, req CreateFolderRequest) (CreateFolderResponse, error)
+	GetFolderContents(ctx context.Context, req GetFolderContentsRequest) (GetFolderContentsResponse, error)
 	//RetrieveFolderMetadata()
 	//UploadFile()
 	//RetrieveFileMetadata()
 	//DownloadFile()
 }
 
+type Config struct {
+	sdk.Config
+	APIKey string
+	AccID  int
+}
+
 type clientImpl struct {
 	client sdk.Client
 	APIKey string
+	AccID  int
 }
 
 func New(cfg Config) *clientImpl {
@@ -26,5 +37,12 @@ func New(cfg Config) *clientImpl {
 			URL:     cfg.URL,
 		}),
 		APIKey: cfg.APIKey,
+		AccID:  cfg.AccID,
 	}
+}
+
+func (c *clientImpl) headers(headers map[string]string) map[string]string {
+	return util.CombineMapString(map[string]string{
+		"Authorization": fmt.Sprint("APIKey ", c.APIKey),
+	}, headers)
 }
